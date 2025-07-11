6029ecfabf82408f7d690b0a83f43fc9690c986b
Muneer Ali <alimuneerali245@gmail.com>
2025-07-11 07:38:39 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,5 @@
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import { ClerkProvider } from '@clerk/clerk-react';
-import { BrowserRouter } from 'react-router-dom';
-
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-createRoot(document.getElementById('root')!).render(
-  <ClerkProvider publishableKey={clerkPubKey}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ClerkProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
